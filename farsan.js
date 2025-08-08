c9f42723e9dc530b9cf883ac349238d8b5d3d3bd
Farsan K <farsanfarsank41@gmail.com>
2025-08-08 20:38:08 +0530

    Update farsan.js

--- farsan.js
+++ farsan.js
@@ -57,7 +57,7 @@ window.onscroll = () => {
 
 
         const typed = new Typed('.text',{
-            strings: ['Machine Learning Engineering...!','Data Scientist...!', 'Data Analyst...!'],
+            strings: ['Data Scientist...!', 'Data Analyst...!', 'Machine Learning Engineering...!'],
             typeSpeed: 100,
             backSpeed: 100,
             backDelay: 1000,
@@ -83,6 +83,7 @@ window.onscroll = () => {
 
       
         
+
 
 
 
